4f3033b87be42f025cd5ee9783d151679fc4e41f
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2023-05-16 22:57:28 +0200

    Actualizar fun-adquirir (1) (1).js

--- plugins/fun-adquirir (1) (1).js
+++ plugins/fun-adquirir (1) (1).js
@@ -8,17 +8,21 @@ while (b === a)
 m.reply(`*Precios del bot* 
 
 
+*Precios del bot* 
+
+
 _*PRECIOS*_
 
-• 1 BOT 2USD
-• 2 BOT 4USD
-• 3 BOT 5USD
-• 4 BOT 7USD
-• 5 BOT 8USD
+• 1 BOT 2 EUROS
+• 2 BOT 4 EUROS
+• 3 BOT 5 EUROS
+• 4 BOT 7 EUROS 
+• 5 BOT 9 EUROS
+• 6 BOT 10 EUROS
 
 • BOT PERSONALIZADOS 20USD
 
-*si te interesa aqui te dejo mi ko-fi en donar pones los precios que te dije osea la cantidad de bots que quieres https://ko-fi.com/iambuitre por ko-fi puedes pagar por targeta o Paypal✅luego de hacer el pago le envías comprobante a mi creador: http://Wa.me/34604135921 tambien puedes pagar por medio de Paypal: https://www.paypal.me/maicolmurillo152*`, null, {
+*si te interesa aqui te dejo mi ko-fi en donar pones los precios que te dije osea la cantidad de bots que quieres https://ko-fi.com/iambuitre por ko-fi puedes pagar por targeta o Paypal✅luego de hacer el pago le envías comprobante a mi creador: http://Wa.me/523316968621 tambien puedes pagar por medio de Paypal: https://www.paypal.me/Buitre01*`, null, {
 mentions: [a, b]
 })}
 handler.help = ['adquerir']
